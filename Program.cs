b35c0804676289142ef1708031be3c467cc01174
Depaa <defajati0@gmail.com>
2025-08-06 13:06:25 +0700

    app cek umur c# console

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class BaskaraDepaSaputra
+{
+    public static void Main(string[] args)
+    {
+        int? umur;
+        Console.Write("Masukan Umur Anda: ");
+        umur = int.Parse(Console.ReadLine());
+
+        if (umur > 0 && umur <= 5)
+        {
+            Console.WriteLine("Balita");
+        }
+        else if (umur > 6 && umur <= 12)
+        {
+            Console.WriteLine("Anak - Anak");
+        }
+        else if (umur > 13 && umur <= 17)
+        {
+            Console.WriteLine("Remaja");
+        }
+        else if (umur > 18 && umur <= 59)
+        {
+            Console.WriteLine("Dewasa");
+        }
+        else
+        {
+            Console.WriteLine("Lansia");
+        }
+        
+    }
+}
