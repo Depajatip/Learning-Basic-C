47c9bd0637dee18528adb103f5ecce227a7bc501
Depaa <defajati0@gmail.com>
2025-08-06 13:23:49 +0700

    logika pengecekan angka bulat ganjil genap

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class BaskaraDepaSaputra
+{
+    public static void Main(string[] args)
+    {
+        int? angka;
+        Console.Write("Masukan Angka Bulat: ");
+        angka = int.Parse(Console.ReadLine());
+
+        if (angka % 2 == 0)
+        {
+            Console.WriteLine($"Angka {angka} adalah bilangan Genap");
+        }
+        else
+        {
+            Console.WriteLine($"Angka {angka} adalah bilangan Ganjil");
+        }
+    }
+}
