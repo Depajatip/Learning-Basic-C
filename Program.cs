7ed3054e17ebf1447eda6cbf05fdfaaec2091bc3
Depaa <defajati0@gmail.com>
2025-08-06 13:32:10 +0700

    logika looping for loop and while loop

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class BaskaraDepaSaputra
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Menggunakan for loop: ");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Menggunakan while loop");
+        int j = 1;
+        while (j <= 10)
+        {
+            Console.WriteLine(j);
+            j++;
+        }
+    }
+}
