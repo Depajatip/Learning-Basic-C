6cbd9530d5b4c3fbb131fb945aa5e8a1a6549b0c
Depaa <defajati0@gmail.com>
2025-08-07 12:48:12 +0700

    logika looping jumlah

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class BaskaraDepaSaputra
+{
+    public static void Main(string[] args)
+    {
+       Console.Write("Masukan Nilai N: ");
+        string? input = Console.ReadLine();
+
+        if (int.TryParse(input, out int N))
+        {
+            int total = 0;
+            Console.WriteLine("Bilangan n anda adalah " + N);
+            Console.Write("Hasil Penjumlahan 1 sampai " + N + " adalah ");
+
+            for (int i = 1; i <= N; i++)
+            {
+                total += i;
+            }
+
+            Console.WriteLine("= " + total);
+        }
+        else
+        {
+            Console.WriteLine("Input tidak valid. Harap masukkan bilangan bulat.");
+        }
+    }
+}
