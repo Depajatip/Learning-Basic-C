3e584a1bf9b31342068f897682e48224b480aa37
Depaa <defajati0@gmail.com>
2025-08-07 12:59:35 +0700

    logika menghitung index di dalam array

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class BaskaraDepaSaputra
+{
+   static void Main()
+    {
+        string[] arrayNama = new string[] { "Joni", "Meri", "david", "Sari" };
+        Console.WriteLine("Panjang arrayNama = " + arrayNama.Length);
+
+        for (int i = 0; i <= arrayNama.Length; i++)
+        {
+            Console.WriteLine("Elemen dengan indeks " + i + " = " + arrayNama[i]);
+        }
+    }
+}
