581ccf903ba89108a95a1fb8760f2c688c76bb73
Depaa <defajati0@gmail.com>
2025-08-07 11:35:05 +0700

    logika perulangan genap dari input user

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class BaskaraDepaSaputra
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Masukkan batas maksimal bilangan: ");
+        string? input = Console.ReadLine();
+
+        if (int.TryParse(input, out int N))
+        {
+            Console.WriteLine($"Bilangan genap dari 1 sampai {N} adalah:");
+            for (int i = 1; i <= N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Input tidak valid. Harap masukkan bilangan bulat.");
+        }
+    }
+}
