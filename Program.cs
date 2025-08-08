7c8495da943090e2a445ea1339e5e6ac1464900a
Depaa <defajati0@gmail.com>
2025-08-08 11:33:40 +0700

    feat: menambahkan logika moduls dan hasil perbandingan

--- Program.cs
+++ Program.cs
@@ -17,7 +17,9 @@ public class BaskaraDepaSaputra
             Console.WriteLine("2. Pengurangan");
             Console.WriteLine("3. Perkalian");
             Console.WriteLine("4. Pembagian");
-            Console.WriteLine("5. Keluar");
+            Console.WriteLine("5. Modulus");
+            Console.WriteLine("6. Perbandingan");
+            Console.WriteLine("7. Keluar");
 
             Console.Write("Anda akan masuk ke menu: ");
             string pilihan = Console.ReadLine();
@@ -41,6 +43,26 @@ public class BaskaraDepaSaputra
             {
                 int jawaban = numberOne / numberTwo;
                 return jawaban;
+            }
+            static int sisaBagi(int numberOne, int numberTwo)
+            {
+                int jawaban = numberOne % numberTwo;
+                return jawaban;
+            }
+            static string perbandingan(int numberOne, int numberTwo)
+            {
+                if (numberOne > numberTwo)
+                {
+                    return $"{numberOne} > {numberTwo} :: {numberOne} lebih besar dari {numberTwo}";
+                }
+                else if (numberOne < numberTwo)
+                {
+                    return $"{numberOne} < {numberTwo} :: {numberOne} lebih kecil dari {numberTwo}";
+                }
+                else
+                {
+                    return $"{numberOne} = {numberTwo}";
+                }
             }
 
 
@@ -100,9 +122,30 @@ public class BaskaraDepaSaputra
 
                     break;
                 case "5":
-                    Console.WriteLine("Keluar dari program...");
-                    return;
+                    Console.WriteLine("== Anda masuk ke menu sisa bagi");
+                    Console.Write("Masukan angka ke 1: ");
+                    int num1sisa = int.Parse(Console.ReadLine());
+                    Console.Write("Masukan angka ke 2: ");
+                    int num2sisa = int.Parse(Console.ReadLine());
 
+                    int sisa = sisaBagi(num1sisa, num2sisa);
+
+                    Console.WriteLine($"hasil dari sisa pembagian: {num1sisa} % {num2sisa} adalah: {sisa}");
+
+                    break;
+                case "6":
+                    Console.WriteLine("== Anda masuk ke menu Perbandingan");
+                    Console.Write("Masukan angka ke 1: ");
+                    int num1perbandingan = int.Parse(Console.ReadLine());
+                    Console.Write("Masukan angka ke 2: ");
+                    int num2perbandingan = int.Parse(Console.ReadLine());
+
+                    string hasil = perbandingan(num1perbandingan, num2perbandingan);
+                    Console.WriteLine($"Hasil perbandingan: {hasil}");
+                    break;
+                case "7":
+                    Console.WriteLine("keluar dari program..");
+                    return;
                 default:
                     Console.WriteLine("");
                     break;
