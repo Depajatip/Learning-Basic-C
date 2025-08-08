a438e9ddc3237fc4d8864b5b4dfc1b90da87b18b
Depaa <defajati0@gmail.com>
2025-08-08 10:03:55 +0700

    struktur method static void and string

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class BaskaraDepaSaputra
+{
+    static void Main(string[] args)
+    {
+        PrintMethodVoid();
+        Console.WriteLine(PrintMethodReturn());
+    }
+
+    static void PrintMethodVoid()
+    {
+        Console.WriteLine("Ini adalah method void");
+    }
+    static String PrintMethodReturn()
+    {
+        return "Ini adalah method return String";
+    }
+}
